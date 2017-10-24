6e9b5e107ad04028273bce5c88b7e012658fe6f3
hanoko <celine.morin.42@gmail.com>
2017-10-24 09:39:49 +0200

    my-simple-stack-1 Ajouter des fonctionnalités

--- src/main/java/deqo/cmor/mysimplestack/Item.java
+++ src/main/java/deqo/cmor/mysimplestack/Item.java
@@ -3,15 +3,12 @@ package deqo.cmor.mysimplestack;
 public class Item {
 
     private int value;
-
     public Item(int value) {
         this.value = value;
     }
-
     public void setValue(int value) {
         this.value = value;
     }
-
     public int getValue() {
         return value;
     }
